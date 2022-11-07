d437d56a87d29d05b6ec30991b19addf3ce82b00
AntoninaTrapsh <antrapsh@gmail.com>
2022-11-07 19:05:56 +0300

    feat: added base news-item design

--- src/components/pages/news-item-page/components/comment/comment.jsx
+++ src/components/pages/news-item-page/components/comment/comment.jsx
@@ -1,7 +1,8 @@
-import {Avatar, Comment as AntComment, Tooltip} from "antd";
+import {Avatar, Comment as AntComment, Tag, Tooltip} from "antd";
 import React from "react";
 import { fetchChildrenComments} from "../../../../../store/actionsCreators";
 import {useDispatch} from "react-redux";
+import {DeleteOutlined, StopOutlined} from "@ant-design/icons";
 
 const Comment = ({comment}) => {
     const dispatch = useDispatch();
@@ -14,12 +15,14 @@ const Comment = ({comment}) => {
     return (
         <AntComment
             key={comment.id}
-            actions={[comment?.kids && <span key="comment-nested-reply-to" onClick={handleShowMoreComments}>Показать ветку</span>]}
-            author={<a>{comment.by}</a>}
+            actions={comment?.kids && [ <span key="comment-nested-reply-to" onClick={handleShowMoreComments}>Show replies</span>]}
+            author={<a>{comment.by ? comment.by : "unknown"}</a>}
             avatar={<Avatar src="https://joeschmoe.io/api/v1/random" alt={comment.by}/>}
             content={
                 <p>
-                    {comment.text}
+                    {comment.text ? comment.text : null}
+                    {comment.deleted ? <Tag color="error">This comment was deleted. <DeleteOutlined /></Tag> : null}
+                    {comment.dead ? <Tag color="error">This comment was killed by software, user flags, or moderators. <StopOutlined /></Tag> : null}
                 </p>
             }
             datetime={
--- src/components/pages/news-item-page/news-item-page.jsx
+++ src/components/pages/news-item-page/news-item-page.jsx
@@ -4,6 +4,8 @@ import {useDispatch, useSelector} from "react-redux";
 import {fetchActiveNewsComments, fetchActiveNewsItemById} from "../../../store/actionsCreators";
 import {selectActiveNewsItem} from "../../../store/selectors";
 import Comment from "./components/comment/comment"
+import {Button, Descriptions, PageHeader} from "antd";
+import styles from "./news-item-page.module.css"
 
 const NewsItemPage = () => {
     let {id} = useParams();
@@ -22,13 +24,34 @@ const NewsItemPage = () => {
     console.log(news);
 
     return (
-        <div>
-            {
-                news.comments.map((comment) => {
-                    return <Comment key={comment.id} comment={comment}/>
-                })
-            }
-        </div>);
+        <section>
+            <PageHeader
+                className="site-page-header"
+                onBack={() => window.history.back()}
+                title={news.info.title}
+                subTitle={news.info.time}
+                extra={[
+                    <Button key="1" type="primary">
+                        <a href={news.info.url}>Follow the link</a>
+                    </Button>,
+                ]}
+            >
+                <Descriptions size="small" column={3}>
+                    <Descriptions.Item label="Author">{news.info.by}</Descriptions.Item>
+                    <Descriptions.Item label="Total number of comments">{news.info.descendants}</Descriptions.Item>
+                </Descriptions>
+            </PageHeader>
+            <div className={styles['news-item__comments']}>
+                <h3>
+                    Comments
+                </h3>
+                {
+                    news.comments.map((comment) => {
+                        return <Comment key={comment.id} comment={comment}/>
+                    })
+                }
+            </div>
+        </section>);
 }
 
 export default NewsItemPage;
